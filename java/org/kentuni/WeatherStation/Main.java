0692171934a02a012fcd8edb9aa1ae6db93f9ea4
Harry Clarke <harry-clarke@outlook.com>
2016-12-29 22:51:56 +0000

    Added Main class/method for quick testing.

---
+++ java/org/kentuni/WeatherStation/Main.java
@@ -0,0 +1,49 @@
+package org.kentuni.WeatherStation;
+
+import com.pi4j.io.gpio.GpioController;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYDataset;
+import org.kentuni.WeatherStation.Drivers.Gpio;
+
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		SwingUtilities.invokeLater(Main::demoJFreeChart);
+		final GpioController gpio = Gpio.getController();
+	}
+
+	public static void demoJFreeChart() {
+		JFrame frame = new JFrame("Charts");
+
+		frame.setSize(600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		XYDataset ds = createDataset();
+		JFreeChart chart = ChartFactory.createXYLineChart("Test Chart",
+				"x", "y", ds, PlotOrientation.VERTICAL, true, true,
+				false);
+
+		ChartPanel cp = new ChartPanel(chart);
+
+		frame.getContentPane().add(cp);
+	}
+
+	private static XYDataset createDataset() {
+
+		DefaultXYDataset ds = new DefaultXYDataset();
+
+		double[][] data = { {0.1, 0.2, 0.3}, {1, 2, 3} };
+
+		ds.addSeries("series1", data);
+
+		return ds;
+	}
+}
