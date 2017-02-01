09e45054cd409017edbbb3ca6dcdbc3aa38741d1
Harry Clarke <harry-clarke@outlook.com>
2017-02-01 04:48:04 +0000

    Added parts of the wind vane. Still need a driver.

---
+++ src/org/kentuni/WeatherStation/Implementations/PiWindVane.java
@@ -0,0 +1,16 @@
+package org.kentuni.WeatherStation.Implementations;
+
+import org.kentuni.WeatherStation.Sensors.WindVane;
+import org.kentuni.WeatherStation.Units.WindDirection;
+
+public class PiWindVane implements WindVane {
+
+    public PiWindVane() {
+        // Todo: Hook up to driver.
+    }
+
+    @Override
+    public WindDirection getWindDirection() {
+        return null;
+    }
+}
