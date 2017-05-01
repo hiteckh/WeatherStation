24a7cde4151e7e698331271ee48f0b54b94b7dc8
Harry Clarke <harry-clarke@outlook.com>
2017-05-01 16:39:41 +0100

    Fixed ordering bug in PiWindVane.

--- src/org/bluej/WeatherStation/Implementations/PiWindVaneSensor.java
+++ src/org/bluej/WeatherStation/Implementations/PiWindVaneSensor.java
@@ -1,31 +1,60 @@
 package org.bluej.WeatherStation.Implementations;
 
 import org.bluej.WeatherStation.Drivers.MCP3427;
+import org.bluej.WeatherStation.Sensors.SensorException;
 import org.bluej.WeatherStation.Sensors.WindVaneSensor;
 import org.bluej.WeatherStation.Units.WindDirection;
 
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * Implementation of the wind vane.
+ * Uses {@link MCP3427}.
+ */
 public final class PiWindVaneSensor implements WindVaneSensor {
 
+	/**
+	 * The number of the air quality sensor bus.
+	 */
 	private static final int BUS_NUM = 1;
+
+	/**
+	 * The address of the air quality sensor.
+	 */
 	private static final int ADDRESS = 0x69;
 
-	private static MCP3427 DRIVER = null;
+	/**
+	 * Singleton instance.
+	 */
+	private static MCP3427 driverSingleton = null;
 
-	private static synchronized MCP3427 getDriver() throws IOException {
-		if (DRIVER == null)
-			DRIVER = new MCP3427(BUS_NUM, ADDRESS);
-		return DRIVER;
+	/**
+	 * @return The singleton.
+	 */
+	private static synchronized MCP3427 getDriver() {
+		if (driverSingleton == null) {
+			try {
+				driverSingleton = new MCP3427(BUS_NUM, ADDRESS);
+			} catch (final IOException e) {
+				throw new SensorException(e);
+			}
+		}
+		return driverSingleton;
 	}
 
 	public static double V_IN = 3.268;
 	public static int V_DIVIDER = 75000;
 
+	/**
+	 * Local instance of the driver.
+	 */
 	private final MCP3427 driver;
 
-	public PiWindVaneSensor() throws IOException {
+	/**
+	 * Constructor.
+	 */
+	public PiWindVaneSensor() {
 		driver = getDriver();
 	}
 
@@ -34,14 +63,17 @@ public final class PiWindVaneSensor implements WindVaneSensor {
 	 * @param adc The ADC value.
 	 * @return The angle.
 	 */
-	private static double angleFromADC (final int adc) {
-		for(final Direction dir : Direction.values()) {
+	private static double angleFromADC(final int adc) {
+		for (final Direction dir : Direction.values()) {
 			if (adc >= dir.adcMin && adc <= dir.adcMax)
 				return dir.angle;
 		}
 		throw new IllegalStateException();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public WindDirection getWindDirection() {
 		final int adc;
@@ -55,53 +87,77 @@ public final class PiWindVaneSensor implements WindVaneSensor {
 		final WindDirection direction = new WindDirection(angle);
 		return direction;
 	}
-}
 
-enum Direction implements Comparable<Direction> {
-	 N(0.0, 33000),
-	 NNE(22.5, 6570),
-	 NE(45.0, 8200),
-	 ENE(67.5, 891),
-	 E(90.0, 1000),
-	 ESE(112.5, 688),
-	 SE(135.0, 2200),
-	 SSE(157.5, 1410),
-	 S(180.0, 3900),
-	 SSW(202.5, 3140),
-	 SW(225.0, 16000),
-	 WSW(247.5, 14120),
-	 W(270.0, 120000),
-	 WNW(292.5, 42120),
-	 NW(315.0, 64900),
-	 NNW(337.5, 21880);
+	/**
+	 * Represents the different directions.
+	 */
+	private enum Direction {
+		N(0.0, 33000),
+		NNE(22.5, 6570),
+		NE(45.0, 8200),
+		ENE(67.5, 891),
+		E(90.0, 1000),
+		ESE(112.5, 688),
+		SE(135.0, 2200),
+		SSE(157.5, 1410),
+		S(180.0, 3900),
+		SSW(202.5, 3140),
+		SW(225.0, 16000),
+		WSW(247.5, 14120),
+		W(270.0, 120000),
+		WNW(292.5, 42120),
+		NW(315.0, 64900),
+		NNW(337.5, 21880);
 
-	public static final Direction[] DIRECTIONS;
+		/**
+		 * An ordered set of directions.
+		 */
+		public static final Direction[] DIRECTIONS;
 
-	static {
-		DIRECTIONS = Direction.values();
-		Arrays.sort(DIRECTIONS);
-		for (int i=1; i<DIRECTIONS.length; i++) {
-			Direction lower = DIRECTIONS[i-1];
-			Direction upper = DIRECTIONS[i];
+		static {
+			DIRECTIONS = Direction.values();
+			// Sort the directions by their ADC value.
+			Arrays.sort(DIRECTIONS, (a, b) -> a.adc - b.adc);
+			for (int i = 1; i < DIRECTIONS.length; i++) {
+				Direction lower = DIRECTIONS[i - 1];
+				Direction upper = DIRECTIONS[i];
 
-			final int halfWay = (lower.adc + upper.adc) / 2;
+				final int halfWay = (lower.adc + upper.adc) / 2;
 
-			lower.adcMax = halfWay;
-			upper.adcMin = halfWay + 1;
+				lower.adcMax = halfWay;
+				upper.adcMin = halfWay + 1;
+			}
+
+			DIRECTIONS[0].adcMin = 0;
+			DIRECTIONS[DIRECTIONS.length - 1].adcMax = MCP3427.MAX;
 		}
 
-		DIRECTIONS[0].adcMin = 0;
-		DIRECTIONS[DIRECTIONS.length - 1].adcMax = MCP3427.MAX;
-	}
+		/**
+		 * The angle that this direction represents.
+		 * Midpoint.
+		 */
+		private final double angle;
 
-	public final double angle;
-	public final int adc;
-	public int adcMax, adcMin;
+		/**
+		 * The corresponding mid-point ADC value.
+		 */
+		private final int adc;
 
+		/**
+		 * Upper and lower bounds for this ADC value.
+		 * Only naturally set in {@link #DIRECTIONS}.
+		 */
+		private int adcMax, adcMin;
 
-	Direction(final double angle, final int ohms) {
-		this.angle = angle;
-		final double vOut = (int) PiWindVaneSensor.V_IN * ohms / (PiWindVaneSensor.V_DIVIDER + ohms);
-		this.adc = (int) (vOut * MCP3427.MAX / MCP3427.VREF);
+		/**
+		 * Constructor.
+		 * @param angle {@link #angle}.
+		 * @param ohms Corresponding resistance for representing this angle on the wind vane hardware.
+		 */
+		Direction(final double angle, final int ohms) {
+			this.angle = angle;
+			final double vOut = PiWindVaneSensor.V_IN * ohms / (double) (PiWindVaneSensor.V_DIVIDER + ohms);
+			this.adc = (int) (vOut * MCP3427.MAX / MCP3427.VREF);
+		}
 	}
 }
