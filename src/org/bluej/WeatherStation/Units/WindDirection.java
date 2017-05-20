0515d2038fc2fb63901060e11b6e48b0095add9c
Harry Clarke <harry-clarke@outlook.com>
2017-05-20 14:25:26 +0100

    Fixed mistake with wind direction unit class.

--- src/org/bluej/WeatherStation/Units/WindDirection.java
+++ src/org/bluej/WeatherStation/Units/WindDirection.java
@@ -25,17 +25,17 @@ public class WindDirection {
 
         if (degrees >= 338 || degrees < 23) {
             compassDirection = Compass.NORTH;
-        } else if (degrees >= 23 || degrees < 68) {
+        } else if (degrees >= 23 && degrees < 68) {
             compassDirection = Compass.NORTH_EAST;
-        } else if (degrees >= 68 || degrees < 113) {
+        } else if (degrees >= 68 && degrees < 113) {
             compassDirection = Compass.EAST;
-        } else if (degrees >= 113 || degrees < 158) {
+        } else if (degrees >= 113 && degrees < 158) {
             compassDirection = Compass.SOUTH_EAST;
-        } else if (degrees >= 158 || degrees < 203) {
+        } else if (degrees >= 158 && degrees < 203) {
             compassDirection = Compass.SOUTH;
-        } else if (degrees >= 203 || degrees < 248) {
+        } else if (degrees >= 203 && degrees < 248) {
             compassDirection = Compass.SOUTH_WEST;
-        } else if (degrees >= 248 || degrees < 293) {
+        } else if (degrees >= 248 && degrees < 293) {
             compassDirection = Compass.WEST;
         } else {
             compassDirection = Compass.NORTH_WEST;
